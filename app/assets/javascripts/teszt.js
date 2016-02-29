64bca41cd66e4e56a6cb47dc3f4b4f6e30400b06
BalazsMarton <balazsmarton90.joomla@gmail.com>
2016-02-29 03:28:31 +0100

    logo fixek button toggle javascript

--- app/assets/javascripts/teszt.js
+++ app/assets/javascripts/teszt.js
@@ -2,3 +2,10 @@
 $(document).ready(function() {
     $("#lightGallery").lightGallery(); 
   });
+
+$(document).ready(function() {
+ $('.navbar-toggle').on("click", function (event) {
+    $('.navbar-toggle').toggleClass('rotate');
+    $('.navbar-toggle').toggleClass('rotate2');
+ }); 
+});
