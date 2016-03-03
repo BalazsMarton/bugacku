2585906646848626064c2d62b62f9ce050b670ef
BalazsMarton <balazsmarton90.joomla@gmail.com>
2016-03-03 04:09:25 +0100

    javascript teszt

--- app/assets/javascripts/teszt.js
+++ app/assets/javascripts/teszt.js
@@ -1,17 +1,14 @@
+$(function() {
 
-$(document).ready(function() {
-    $("#lightGallery").lightGallery(); 
-  });
+	$("#lightGallery").lightGallery();
 
-$(document).ready(function() {
- $('.navbar-toggle').on("click", function (event) {
+ 	$('.navbar-toggle').on("click", function (event) {
     $('.navbar-toggle').toggleClass('rotate');
     $('.navbar-toggle').toggleClass('rotate2');
- }); 
-});
+ 	});
 
-$(document).ready(function(){
-    $(".nav-tabs a").click(function(){
+ 	$(".nav-tabs a").click(function(){
         $(this).tab('show');
     });
+
 });
